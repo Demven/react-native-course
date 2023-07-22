66e5654d8acd9a0a3109c45ad7ba27c617939b5b
dsalnikov <dsalnikov@brightcove.com>
2023-07-21 22:05:25 -0400

    Basic form

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+} from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+
+  function onPress () {
+    console.info(text);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.app}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='Your course goal'
+          onChangeText={setText}
+          value={text}
+        />
+
+        <Button
+          title='Add goal'
+          onPress={onPress}
+        />
+      </View>
+
+      <View style={styles.goalsContainer}>
+        <Text>List of goals</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    marginBottom: 24,
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    width: '70%',
+    padding: 8,
+    marginRight: 8,
+    borderWidth: 1,
+    borderColor: '#cccccc',
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
